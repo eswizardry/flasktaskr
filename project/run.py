52c558c555b2446e3843622fbcf8da2044aebe08
eswizardry <edrit011@gmail.com>
2016-09-22 09:30:49 +0700

    Adding main style

---
+++ project/run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File: run.py
+# @Author: eswizardry
+# @Date:   2016-09-22 09:20:30
+# @Last Modified by:   eswizardry
+# @Last Modified time: 2016-09-22 09:21:41
+
+from views import app
+app.run(debug=True)
